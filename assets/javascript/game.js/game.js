f850a1ab365db04a7ca512f66d1b43e1d312717c
breal5000 <34284810+breal5000@users.noreply.github.com>
2018-03-07 20:28:08 -0800

    first updates

---
+++ assets/javascript/game.js/game.js
@@ -0,0 +1,11 @@
+// array of cycling words //
+const words = ["peloton", "wheel", "cog", "crankset", "jersey", "breakaway", "watts", "pedals", "ascent", "decend", "deraileur"];
+// choose randomly //
+let randNum = Math.floor(math.random() * words.length);
+let chosenWord = word[randNum];
+console.log(chosenWord);
+// create underscore lenght for word //
+// get users guess //
+// check to see if guess is right //
+// if right push to right //
+// if wrong push to wrong //
