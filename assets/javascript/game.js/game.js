ea7f6f4b2c0556d8ba681c383b091db8a45e0d7b
breal5000 <34284810+breal5000@users.noreply.github.com>
2018-03-08 17:32:49 -0800

    a couple of tweaks

--- assets/javascript/game.js/game.js
+++ assets/javascript/game.js/game.js
@@ -1,11 +1,13 @@
-// array of cycling words //
-const words = ["peloton", "wheel", "cog", "crankset", "jersey", "breakaway", "watts", "pedals", "ascent", "decend", "deraileur"];
-// choose randomly //
-let randNum = Math.floor(math.random() * words.length);
-let chosenWord = word[randNum];
-console.log(chosenWord);
-// create underscore lenght for word //
-// get users guess //
-// check to see if guess is right //
-// if right push to right //
-// if wrong push to wrong //
+    // array of cycling words //
+var wordBank = ["peloton", "pace line", "campagnolo", "mavic", "musette", "pannier", "polka-dot jersey", "maillot jaune", "breakaway", "watts", "sprinter", "ascent", "decend", "deraileur"];
+    // assinging variables //
+for (var i=0; i<wordBank.lenght; i++){
+    console.log(wordBank[i]);
+}
+    // begin the game //
+    // create underscore lenght for word //
+    // get users guess //
+    // check to see if guess is right //
+    // if right push to right //
+    // if wrong push to wrong //
+
